1df3ac6e4cbbdf4b2525c113f73e0b613023598d
skyhawkgaming <trvs.wade@gmail.com>
2022-08-07 06:24:48 -0500

    getstaticprops

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,30 +1,27 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import GetCards from '../../components/GetCards';
 import SideBar from '../../components/SideBar';
-import Tables from '../../components/Tables';
 import SignIn from '../../components/SignIn';
-const Home = () => {
-    // const { data: session } = useSession()
+
+
+const Home = ({ post }) => {
+    const resultsPerPage = 10;
+    const totalResults = post.length;
 
     return (
-        <div className="bg-slate-900">
-            <div className="relative flex bg-main-dark-bg">
-                <SideBar />
-                <div
-                    className="fixed right-4 bottom-4"
-                    style={{ zIndex: '1000' }}
-                ></div>
-                <div className="w-full min-h-screen pl-20 bg-main-bg">
-                    <div>
-                        <div className="text-center">
-                            <GetCards />
-                            <Tables />
-                            <SignIn />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     );
 };
+
+export async function getStaticProps() {
+    const res = await fetch('http://78.108.218.94:25837/api/members');
+    const post = await res.json();
+    return {
+        props: {
+            post,
+        },
+    };
+}
 
 export default Home;

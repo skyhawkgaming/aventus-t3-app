c62ae5959a1b944cec3cf1f21ad24c803d8f9e13
skyhawkgaming <trvs.wade@gmail.com>
2022-08-07 06:52:23 -0500

    removed table

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,34 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import GetCards from '../../components/GetCards';
 import SideBar from '../../components/SideBar';
 import SignIn from '../../components/SignIn';
 
-
-const Home = ({ post }) => {
-    const resultsPerPage = 10;
-    const totalResults = post.length;
+function Home(post) {
+    // const { data: session } = useSession()
 
     return (
-
+        <div className="bg-slate-900">
+            <div className="relative flex bg-main-dark-bg">
+                <SideBar />
+                <div
+                    className="fixed right-4 bottom-4"
+                    style={{ zIndex: '1000' }}
+                ></div>
+                <div className="w-full min-h-screen pl-20 bg-main-bg">
+                    <div>
+                        <div className="text-center">
+                            <GetCards />
+                            <SignIn />
+                            <div className="text-white">
+                                {JSON.stringify(post)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-};
+}
 
 export async function getStaticProps() {
     const res = await fetch('http://78.108.218.94:25837/api/members');

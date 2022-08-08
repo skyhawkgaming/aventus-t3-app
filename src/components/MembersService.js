ee0cc9e2b47cf38927e603020261e0d16a8477ae
Dylan White <Whitedylan7@gmail.com>
2022-08-08 00:24:20 -0500

    zzz

---
+++ src/components/MembersService.js
@@ -0,0 +1,14 @@
+
+export class MembersService {
+getPost() {
+    const https = require("https");
+  const agent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+  const res = fetch("https://78.108.218.94:25837/api/members", {
+    agent,
+  });
+  const post = res;
+    return post;
+}
+}

f08b29c7bda4994eaa39cfa48abdb34f95e1627d
Dylan White <Whitedylan7@gmail.com>
2022-08-09 00:15:08 -0500

    .

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,18 +1,9 @@
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
 
-import { Button } from "primereact/button";
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
-import { Dropdown } from "primereact/dropdown";
-import { InputText } from "primereact/inputtext";
 import { Ripple } from "primereact/ripple";
 import { classNames } from "primereact/utils";
-
-import { BsArrowDownLeftCircle } from "react-icons/bs";
-
-import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
+import NextTable from "../components/table/NextTable";
 
 import {
   BsCalendarEventFill,
